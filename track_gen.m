d799ad665ec79fa87a4552b0df0efa2021684053
d-carb <danielcarbonell22@gmail.com>
2019-02-22 02:20:42 +0000

    clean up code

--- track_gen.m
+++ track_gen.m
@@ -6,37 +6,31 @@
 %% Parameters from trajectory sim
 speed_at_steps = csvread('speed_at_steps.csv');
 time_at_steps = csvread('time_at_steps.csv');
+
 %% Parameters for track and wheel
 r = 0.04;
 max_step = 0.001;
 sample_size = 666;
-
-% tb = (sqrt(2*r*step_size))/v;
-tb = zeros(1,size(speed_at_steps,2));
-% step_size = -max_step + (max_step+max_step)*rand(350,1);
 step_size = (max_step).*rand(350,1) % Random step between 0 & 1mm
 A = [1,-1];
 rand_neg = A(randi([1,2],1,sample_size));
 
-% % Testing
+%% Initial values & blank arrays
+tb = zeros(1,size(speed_at_steps,2));
 run_time = zeros(1,sample_size);
 run_disp = zeros(1,sample_size);
-
-% Full Arr 2x speed at steps
-% run_time = zeros(1,2*size(speed_at_steps,2));
-% run_disp = zeros(1,2*size(speed_at_steps,2));
 prev_time = 0;
 prev_disp = 0;
 
+%% Main loop
 for i=1:sample_size 
-% for i=1:2*size(speed_at_steps,2)
     
-    % set vals at time of impact
+    % set values at time of impact
     if mod(i,2)==1
         run_time(i) = time_at_steps(i/2 + 0.5);
         run_disp(i) = prev_disp;
    
-    % set vals at time of impact + delay
+    % set values at time of impact + rise time delay
     else
         tb = (sqrt(2*r*step_size(i/2)))/(speed_at_steps(i/2));
         run_time(i) = prev_time + tb;
@@ -50,15 +44,11 @@ end
 
 [lin_t, lin_x]=interpolate(run_time, run_disp);
 
+%% Data logging
 
 % figure
-% plot(t, x,'o',xq,vq,'-'); % dot
-plot(run_time, run_disp, lin_t, lin_x,'-'); % no dot
-% plot(xq, vq)
+% plot(t, x,'o',xq,vq,'-'); % with dots
+plot(run_time, run_disp, lin_t, lin_x,'-'); % no dots
 xlabel('Run Time (s)')
 ylabel('Vertical Displacement (m)')
 legend('Samples','Linear Interpolation');
-
-% plot(run_time,run_disp,'o');
-% xlabel('Time');
-% ylabel('Displ');
