8c323becaf97263c607918cdd26e387ca76f5bd1
d-carb <danielcarbonell22@gmail.com>
2019-03-14 14:31:20 +0000

    align speed during interpolation

--- interpolate.m
+++ interpolate.m
@@ -3,7 +3,27 @@
 % HYPED, Technical Director
 % Interpolate the values in time given initial and end conditions. 
 
-function [tq, xq] = interpolate(t, x)
+% function [tq, xq] = interpolate(t, x)
+% 
+% %t(end);
+% 
+% % Interpolated time resolution
+% tq = (0:0.0001:t(end));
+% 
+% % Interpolated displacement value
+% xq = interpn(t, x,tq,'linear');
+% 
+% % vq=0;
+% % vq = interpn(t, v,tq,'linear');
+% % figure
+% % % plot(t, x,'o',xq,vq,'-'); % dot
+% % plot(t, x,tq,xq,'-'); % no dot
+% % % plot(xq, vq)
+% % legend('Samples','Linear Interpolation');
+% 
+% end
+
+function [tq, xq, vq] = interpolate(t, x, ts, vs)
 
 %t(end);
 
@@ -12,7 +32,11 @@ tq = (0:0.0001:t(end));
 
 % Interpolated displacement value
 xq = interpn(t, x,tq,'linear');
+vq = interpn(ts, vs,tq,'linear');
 
+
+% vq=0;
+% vq = interpn(t, v,tq,'linear');
 % figure
 % % plot(t, x,'o',xq,vq,'-'); % dot
 % plot(t, x,tq,xq,'-'); % no dot
