1fdcedc8f27dd7abfa8cc0a3980c0917027d1a4b
d-carb <danielcarbonell22@gmail.com>
2019-03-14 14:32:07 +0000

    interp and show speed and disp on same plot

--- track_gen.m
+++ track_gen.m
@@ -9,9 +9,10 @@ time_at_steps = csvread('time_at_steps.csv');
 
 %% Parameters for track and wheel
 r = 0.04;
-max_step = 0.001;
-sample_size = 666;
-step_size = (max_step).*rand(350,1); % Random step between 0 & 1mm
+max_step = 0.00111;
+min_step = 0.00001
+sample_size = 2*size(speed_at_steps,2);
+step_size = (max_step-min_step).*rand(350,1)+min_step; % Random step between 0 & 1mm
 A = [1,-1];
 rand_neg = A(randi([1,2],1,sample_size));
 
@@ -27,14 +28,19 @@ for i=2:sample_size
     
     % set values at time of impact
     if mod(i,2)==1
+%         i/2+0.5;
         run_time(i) = time_at_steps(i/2 + 0.5);
         run_disp(i) = prev_disp;
+%         run_speed(i) = (run_speed(i-1)+speed_at_steps(i/2 + 0.5))/2;
    
     % set values at time of impact + rise time delay
     else
+%         i/2
         tb = (sqrt(2*r*step_size(i/2)))/(speed_at_steps(i/2));
         run_time(i) = prev_time + tb;
         run_disp(i) = prev_disp + rand_neg(i/2)*step_size(i/2);
+        
+%         run_speed(i) = speed_at_steps(i\2);
     end
     
     prev_disp = run_disp(i);
@@ -42,7 +48,12 @@ for i=2:sample_size
 
 end
 
-[lin_t, lin_x]=interpolate(run_time, run_disp);
+% [lin_t, lin_x]=interpolate(run_time, run_disp);
+
+[lin_t, lin_x, lin_v]=interpolate(run_time, run_disp, time_at_steps, speed_at_steps);
+
+% [lin_t2, lin_v]=interpolate(time_at_steps, speed_at_steps);
+
 
 % initializing time and displacement vectors for SIMULINK model
 var.time = lin_t;
@@ -53,7 +64,13 @@ var.signals.dimensions = 1;
 
 % figure
 % plot(t, x,'o',xq,vq,'-'); % with dots
-plot(run_time, run_disp, lin_t, lin_x,'-'); % no dots
-xlabel('Run Time (s)')
+plot(run_time, run_disp, lin_t, lin_x); % no dots
+yyaxis left;
 ylabel('Vertical Displacement (m)')
+xlabel('Run Time (s)')
+
+yyaxis right;
+plot(lin_t, lin_v,'-');
+ylabel('Run Speed (m/s)')
+
 legend('Samples','Linear Interpolation');
