e1622e5f9b6cbd1e976e173eb28ba7ac1829dd0d
d-carb <danielcarbonell22@gmail.com>
2019-03-24 19:18:05 +0000

    add erm model

---
+++ eccentric_mass.m
@@ -0,0 +1,14 @@
+% 20/03/19
+% Daniel Carbonell
+% HYPED, Technical Director
+% Model of ERM used to reporuduce the vibration environment
+
+%% Global Parameters
+
+sys_mass = 10;
+ecc_mass = 3;
+spr_const = 100;
+dam_const = 10;
+rot_freq = 100;
+sys_freq = sqrt(spr_const/sys_mass);
+
