bbdea9798d4970c8ec3515fc8f24e69b1a632bf6
d-carb <danielcarbonell22@gmail.com>
2019-03-26 10:30:28 +0000

    add functinos to convert frequency

---
+++ hertz_rad.m
@@ -0,0 +1,10 @@
+% 24/03/19
+% Daniel Carbonell
+% HYPED, Technical Director
+% Connverts frequencies from Hz to rad/s
+
+function [freq] = hertz_rad(hertz)
+
+freq = hertz*(2*pi);
+
+end
---
+++ rad_hertz.m
@@ -0,0 +1,13 @@
+% 24/03/19
+% Daniel Carbonell
+% HYPED, Technical Director
+% Connverts frequencies from rad/s to Hz
+
+function [freq] = rad_hertz(rads)
+
+freq = rads/(2*pi);
+
+end
+
+
+
