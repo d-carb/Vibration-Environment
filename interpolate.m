0fe60bc36535c69c041b451b83c06e1d3825c246
d-carb <danielcarbonell22@gmail.com>
2019-04-04 23:08:47 +0100

    change accuracy

--- interpolate.m
+++ interpolate.m
@@ -3,32 +3,12 @@
 % HYPED, Technical Director
 % Interpolate the values in time given initial and end conditions. 
 
-% function [tq, xq] = interpolate(t, x)
-% 
-% %t(end);
-% 
-% % Interpolated time resolution
-% tq = (0:0.0001:t(end));
-% 
-% % Interpolated displacement value
-% xq = interpn(t, x,tq,'linear');
-% 
-% % vq=0;
-% % vq = interpn(t, v,tq,'linear');
-% % figure
-% % % plot(t, x,'o',xq,vq,'-'); % dot
-% % plot(t, x,tq,xq,'-'); % no dot
-% % % plot(xq, vq)
-% % legend('Samples','Linear Interpolation');
-% 
-% end
-
 function [tq, xq, vq] = interpolate(t, x, ts, vs)
 
 %t(end);
 
 % Interpolated time resolution
-tq = (0:0.00001:t(end));
+tq = (0:0.001:t(end));
 
 % Interpolated displacement value
 xq = interpn(t, x,tq,'linear');
